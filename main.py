e259b5d11bb0570e73110f6ac7a9c53d56948cf8
Yash Verma <30554253+AtomicRogue1@users.noreply.github.com>
2022-04-04 21:28:30 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+from playsound import playsound
+
+while True:
+  print("Water Break")
+  playsound('a.wav',False)
+  time.sleep(1.5)
+  playsound('b.wav',False)
+  time.sleep(60*60)
+  
