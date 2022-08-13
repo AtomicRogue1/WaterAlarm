ac3993e02e774d5d68f41f4cae1fc7a02e488370
Yash Verma <30554253+AtomicRogue1@users.noreply.github.com>
2022-08-13 20:35:05 +0530

    Second commit

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import time
 from playsound import playsound
 
 while True:
-  print("Water Break")
-  playsound('a.wav',False)
-  time.sleep(1.5)
-  playsound('b.wav',False)
-  time.sleep(60*60)
-  
+    print("Water Break")
+    playsound('a.wav', False)
+    time.sleep(1.5)
+    playsound('b.wav', False)
+    time.sleep(60*60)
